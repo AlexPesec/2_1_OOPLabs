0c6f33fb81561e8bf8a628306fbd7027e79d9fa0
AlexPesec <43617303+AlexPesec@users.noreply.github.com>
2018-09-27 22:42:35 +0300

    ready

--- Project1/Source.cpp
+++ Project1/Source.cpp
@@ -6,22 +6,22 @@
 #include "Trapeze.h"
 
 int main() {
-	while (true) {
-		std::cout << "Enter number:\n1 - for Foursquare\n2 - for Rectangle\n3 - for Trapeze\n4 - to exit the programm" << std::endl;
-		int n;
-		std::cin >> n;
-		Figure* ptr;
-		if (n == 1) ptr = new FourSquare(2);
-		else if (n == 2) ptr = new Rectangle(2, 2);
-		else if (n == 3) ptr = new Trapeze(2, 2, 2, 2, 2);
-		else if (n == 4) break;
-		else {
-			std::cout << "Bad input, try again\n" << std::endl;
-			continue;
-		}
-		ptr->Print();
-		std::cout << ptr->Square() << std::endl;
-		delete ptr;
-	}
+	Figure* ptr;
+
+	ptr = new FourSquare(2);
+	ptr->Print();
+	std::cout << ptr->Square() << std::endl;
+	delete ptr;
+
+	ptr = new Rectangle(2, 2);
+	ptr->Print();
+	std::cout << ptr->Square() << std::endl;
+	delete ptr;
+
+	ptr = new Trapeze(2, 2, 2, 2, 2);
+	ptr->Print();
+	std::cout << ptr->Square() << std::endl;
+	delete ptr;
+
 	return 0;
 }
